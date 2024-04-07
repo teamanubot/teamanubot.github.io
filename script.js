74816063d8474fec9a91760b588ad1dabcd02d9f
teamanubot <rivaimunthe02@gmail.com>
2024-04-08 02:34:07 +0700

    aku adalah aku

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ yesBtn.addEventListener("click", () => {
         count--;
         if (count === -1) {
             clearInterval(countdownInterval);
-            window.location.href = "home/index.html";
+            window.location.href = "home";
         }
     }, 1000);
 });
