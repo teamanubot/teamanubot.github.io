9c80cace019d7359bf6fbde4cb01dd5e4cfe9e58
teamanubot <rivaimunthe02@gmail.com>
2024-04-08 03:04:20 +0700

    aku adalah aku

---
+++ home/script.js
@@ -0,0 +1,28 @@
+const audio1 = document.getElementById("audio1");
+const audio2 = document.getElementById("audio2");
+
+function showPlayOverlay() {
+    document.getElementById("playOverlay").style.display = "block";
+}
+
+function closeOverlay() {
+    document.getElementById("playOverlay").style.display = "none";
+}
+
+function playOverlay() {
+    audio1.play().catch(error => {
+        showPlayOverlay();
+        if (error.name === 'NotAllowedError') {
+            audio1.requestAutoplay();
+        }
+    });
+    closeOverlay();
+}
+
+audio1.addEventListener("ended", function() {
+    audio2.play();
+});
+
+audio2.addEventListener("ended", function() {
+    audio1.play();
+});
