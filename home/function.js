9f8c4b964becbe502d97bd944af8a0b69b42d414
teamanubot <rivaimunthe02@gmail.com>
2024-04-09 23:25:40 +0700

    aku adalah aku

---
+++ home/function.js
@@ -0,0 +1,30 @@
+const audio1 = document.getElementById("audio1");
+const audio2 = document.getElementById("audio2");
+const audio3 = document.getElementById("audio3");
+const audio4 = document.getElementById("audio4");
+
+function showPlayOverlay() {
+    document.getElementById("playOverlay").style.display = "block";
+}
+
+function closeOverlay() {
+    document.getElementById("playOverlay").style.display = "none";
+}
+
+function playOverlay() {
+    audio1.play().catch(error => {
+        if (error.name === 'NotAllowedError') {
+            audio1.requestAutoplay();
+        }
+    });
+    closeOverlay();
+}
+
+function autoPlayAudio() {
+    audio1.play().catch(error => {
+        showPlayOverlay();
+        if (error.name === 'NotAllowedError') {
+            audio1.requestAutoplay();
+        }
+    });
+}
--- home/script.js
+++ home/script.js
@@ -1,34 +1,3 @@
-const audio1 = document.getElementById("audio1");
-const audio2 = document.getElementById("audio2");
-const audio3 = document.getElementById("audio3");
-const audio4 = document.getElementById("audio4");
-
-function showPlayOverlay() {
-    document.getElementById("playOverlay").style.display = "block";
-}
-
-function closeOverlay() {
-    document.getElementById("playOverlay").style.display = "none";
-}
-
-function playOverlay() {
-    audio1.play().catch(error => {
-        if (error.name === 'NotAllowedError') {
-            audio1.requestAutoplay();
-        }
-    });
-    closeOverlay();
-}
-
-function autoPlayAudio() {
-    audio1.play().catch(error => {
-        showPlayOverlay();
-        if (error.name === 'NotAllowedError') {
-            audio1.requestAutoplay();
-        }
-    });
-}
-
 /* Versi Kuno
 window.onload = function() {
     if (performance.navigation.type === 1 || performance.navigation.type === 2) {
